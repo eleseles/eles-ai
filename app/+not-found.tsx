4d99795f60064f69922ae188661d902334eed23d
Ramazan Eles <66562337+eleseles@users.noreply.github.com>
2025-11-13 12:58:23 +0300

    Update +not-found.tsx

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,10 +1,42 @@
-import React from "react";
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function NotFound() {
+import Colors from "@/constants/colors";
+
+export default function NotFoundScreen() {
   return (
-    <main>
-      <h1>404 - Not Found</h1>
-      <p>Aradığınız sayfa bulunamadı.</p>
-    </main>
+    <>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View style={styles.container}>
+        <Text style={styles.title}>This screen doesn&apos;t exist.</Text>
+
+        <Link href="/" style={styles.link}>
+          <Text style={styles.linkText}>Go to home screen!</Text>
+        </Link>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: Colors.background,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold" as const,
+    color: Colors.text.primary,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: Colors.accent,
+  },
+});
